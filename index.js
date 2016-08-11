56df8975b8abce4fadf643b81755c0a44796746d
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-11 01:23:12 -0700

    Added new index.js in root moved other back to src

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-import SidebarItem from './src/SidebarItem.prototype';
-import SidebarSelector from './src/SidebarSelector';
-import Sidebar from './src/Sidebar';
-export {
-  Sidebar,
-  SidebarItem,
-  SidebarSelector,
-};
+module.exports = require('./dist/bundle');
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import SidebarItem from './src/SidebarItem.prototype';
+import SidebarSelector from './src/SidebarSelector';
+import Sidebar from './src/Sidebar';
+export {
+  Sidebar,
+  SidebarItem,
+  SidebarSelector,
+};
