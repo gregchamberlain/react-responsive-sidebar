bfd75b0c906caec3fd85bb4e9acd914699785c5a
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-11 01:16:00 -0700

    Moved index.js to root folder for exports

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import SidebarItem from './SidebarItem.prototype';
-import SidebarSelector from './SidebarSelector';
-import Sidebar from './Sidebar';
-export {
-  Sidebar,
-  SidebarItem,
-  SidebarSelector,
-};
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = getConfig({
 
   isDev: process.env.NODE_ENV !== "production",
   // entry point for the app
-  in: process.env.NODE_ENV !== "production"?'example/main.js':'src/index.js',
+  in: process.env.NODE_ENV !== "production"?'example/main.js':'index.js',
 
   // Name or full path of output directory
   // commonly named `www` or `public`. This
