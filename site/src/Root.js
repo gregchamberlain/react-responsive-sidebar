2f305fc430a806a4a19c2404b49a0167babed4be
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-12-07 23:58:01 -0800

    Implemented new build/dev setup, removed junk files

--- site/src/Root.js
+++ site/src/Root.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
 import  Dashboard from 'react-icons/lib/fa/dashboard';
 import  User from 'react-icons/lib/fa/user';
 import  Gear from 'react-icons/lib/fa/cog';
-import { Sidebar, SidebarItem } from '../src';
+import { Sidebar, SidebarItem } from '../../src';
 import { Router, Route, browserHistory } from 'react-router'
 
 let items = [
@@ -39,12 +38,14 @@ const DashPage = (props) => <div style={{height: 3000}}><div style={toolbarStyle
 const ProfilePage = (props) => <div><h1>Profile</h1></div>;
 const SettingsPage = (props) => <div><h1>Settings</h1></div>;
 
-render(
+const Root = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="dashboard" component={DashPage} />
       <Route path="profile" component={ProfilePage} />
       <Route path="settings" component={SettingsPage} />
     </Route>
-  </Router>,
-document.getElementById('root'));
+  </Router>
+);
+
+export default Root;
